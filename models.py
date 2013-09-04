38deb9c043d2bf5c016f6f347f392cdcf756f823
Ben LeVeque <ben.leveque@gmail.com>
2013-09-04 09:47:19 -0400

    fixed indentation error

--- models.py
+++ models.py
@@ -14,10 +14,10 @@ from django.utils.encoding import smart_unicode
 # 		verbose_name = u'About page fields'
 		
 class About(models.Model):
-	header = models.CharField( blank=True, max_length=50, help_text=u'header')
-	description = models.TextField( blank=True, max_length=10000, help_text=u'description' )
+    header = models.CharField( blank=True, max_length=50, help_text=u'header')
+    description = models.TextField( blank=True, max_length=10000, help_text=u'description' )
 
-  def __unicode__(self):
+def __unicode__(self):
     return smart_unicode( self.description, u'utf-8', u'replace' )
 
-  # end class About()
+# end class About()
