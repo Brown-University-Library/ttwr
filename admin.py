0da9eac17b69a508fb6426b3bedaec5e746fa7c5
Ben Cail <benjamin_cail@brown.edu>
2014-04-11 14:09:48 -0400

    add new models and register them in the admin

--- admin.py
+++ admin.py
@@ -1,2 +1,13 @@
 # -*- coding: utf-8 -*-
+from django.contrib import admin
+from .models import Biography, Essay
+
+class BiographyAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'trp_id']
+
+class EssayAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'title']
+
+admin.site.register(Biography, BiographyAdmin)
+admin.site.register(Essay, EssayAdmin)
 
--- models.py
+++ models.py
@@ -1,2 +1,15 @@
 from django.db import models
 
+
+class Biography(models.Model):
+
+    name = models.CharField(max_length=254)
+    trp_id = models.CharField(max_length=15)
+    bio = models.TextField()
+
+
+class Essay(models.Model):
+
+    author = models.CharField(max_length=254)
+    title = models.CharField(max_length=254)
+
