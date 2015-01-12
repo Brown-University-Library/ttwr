2ebba965dfea888accd2cd3b17b72491cd52cb86
Ben Cail <benjamin_cail@brown.edu>
2015-01-12 14:52:32 -0500

    make original title language a select field, populated from the pycountry languages (remove languages view/url - not needed)

--- forms.py
+++ forms.py
@@ -1,4 +1,5 @@
 from django import forms
+import pycountry
 from pagedown.widgets import AdminPagedownWidget
 from .models import Biography, Essay, Genre
 from .widgets import AddAnotherWidgetWrapper
@@ -28,9 +29,23 @@ class InscriptionForm(forms.Form):
     text = forms.CharField()
 
 
+def get_language_choices():
+    #list of all the language names and codes
+    #(try to use the 2-letter code, but fall back to 3-letter if needed)
+    langs = [ ('', 'Select a Language'), ('it', 'Italian'), ('fr', 'French'), ('en', 'English') ]
+    for language in pycountry.languages:
+        try:
+            code = language.alpha2
+        except AttributeError:
+            code = language.bibliographic
+        if code not in ['en', 'fr', 'it']:
+            langs.append( (code, language.name) )
+    return langs
+
+
 class AnnotationForm(forms.Form):
     original_title = forms.CharField()
-    original_title_language = forms.CharField(required=False)
+    original_title_language = forms.ChoiceField(required=False, choices=get_language_choices())
     english_title = forms.CharField(required=False)
     genre = forms.ModelChoiceField(required=False, queryset=Genre.objects.all().order_by('text'),
             widget=AddAnotherWidgetWrapper(forms.Select(), Genre))
