f9e51503203a5abfdd5c91dfbe2e10905c27fb2b
Ben Cail <benjamin_cail@brown.edu>
2015-01-13 10:17:54 -0500

    change roles into a model, so users can select from (and add to) a list of models

--- admin.py
+++ admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from .models import Biography, Essay, Genre
+from .models import Biography, Essay, Genre, Role
 from .forms import BiographyModelForm, EssayModelForm
 
 class BiographyAdmin(admin.ModelAdmin):
@@ -14,7 +14,11 @@ class EssayAdmin(admin.ModelAdmin):
 class GenreAdmin(admin.ModelAdmin):
     list_display = ['id', 'text', 'external_id']
 
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'text', 'external_id']
+
 admin.site.register(Biography, BiographyAdmin)
 admin.site.register(Essay, EssayAdmin)
 admin.site.register(Genre, GenreAdmin)
+admin.site.register(Role, GenreAdmin)
 
