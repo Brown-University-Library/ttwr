5f9653bf3ab173a16c3842e5e63f282d4656a155
lucyvk <lucy_van_kleunen@brown.edu>
2016-04-08 11:15:45 -0400

    made changes to essay toggling

--- models.py
+++ models.py
@@ -17,6 +17,7 @@ class Biography(models.Model):
     birth_date = models.CharField(max_length=25, null=True, blank=True, help_text='Optional: enter birth date as yyyy-mm-dd (for sorting and filtering)')
     death_date = models.CharField(max_length=25, null=True, blank=True, help_text='Optional: enter death date as yyyy-mm-dd')
     roles = models.CharField(max_length=254, null=True, blank=True, help_text='Optional: enter roles, separated by a semi-colon')
+    #TO-DO- figure out how to do this data migration -- rolestwo = models.ForeignKey('Role')
     bio = models.TextField()
 
     class Meta:
