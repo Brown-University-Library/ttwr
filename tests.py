0bcb7d8e4c5acb9be1cb2c562079e8575335905d
Ben Cail <benjamin_cail@brown.edu>
2016-08-24 11:34:44 -0400

    add 3 more static view tests

--- tests.py
+++ tests.py
@@ -3,12 +3,31 @@ from django.test import TestCase
 from . import views
 
 
-class TestPageDetail(TestCase):
+class TestStaticViews(TestCase):
 
     def test_index(self):
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'The Theater that was Rome')
+
+    def test_about(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<h2>About</h2>')
+
+    def test_links(self):
+        response = self.client.get(reverse('links'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<li>Links</li>')
+
+    def test_search(self):
+        #this page is static as far as the django view is concerned
+        response = self.client.get(reverse('search_page'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Rome - Search')
+
+
+class TestPageDetail(TestCase):
 
     def test_1(self):
         response = self.client.get(reverse('book_page_viewer', kwargs={'book_id': '230605', 'page_id': '230606'}))
