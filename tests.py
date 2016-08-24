8f16daedf41939a1baaa47c914e815acb4b3c938
Ben Cail <benjamin_cail@brown.edu>
2016-08-24 12:11:59 -0400

    test new_genre view: that user has to be logged in, and a valid get request, and a valid post request

--- tests.py
+++ tests.py
@@ -1,6 +1,19 @@
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-from django.test import TestCase
+from django.test import TestCase, Client
 from . import views
+from . import models
+
+
+def get_auth_client():
+    username = 'someone@brown.edu'
+    password = 'pw'
+    u = User.objects.create_user(username, password=password)
+    auth_client = Client()
+    logged_in = auth_client.login(username=username, password=password)
+    if not logged_in:
+        raise Exception('couldn\'t log in user')
+    return auth_client
 
 
 class TestStaticViews(TestCase):
@@ -53,3 +66,25 @@ class TestPageDetail(TestCase):
         prev_id, next_id = views._get_prev_next_ids({'relations': {'hasPart': hasPart_data}}, 'test:112')
         self.assertEqual(prev_id, '111')
         self.assertEqual(next_id, '113')
+
+
+class TestRecordCreatorViews(TestCase):
+
+    def test_new_genre_auth(self):
+        response = self.client.get(reverse('new_genre'))
+        self.assertRedirects(response, 'http://testserver/rome/login/?next=/rome/genres/new/')
+
+    def test_new_genre(self):
+        auth_client = get_auth_client()
+        response = auth_client.get(reverse('new_genre'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Text')
+
+    def test_new_genre_post(self):
+        auth_client = get_auth_client()
+        self.assertEqual(len(models.Genre.objects.all()), 0)
+        response = auth_client.post(reverse('new_genre'), {'text': 'Book'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'opener.dismissAddAnotherPopup(window, "1", "Book");')
+        self.assertEqual(len(models.Genre.objects.all()), 1)
+        self.assertEqual(models.Genre.objects.all()[0].text, 'Book')
