6c9cc1bae33c47fd10565eabf47c93a71e25cd3c
Ben Cail <benjamin_cail@brown.edu>
2016-08-25 15:56:14 -0400

    remove urls.defaults

--- urls_app.py
+++ urls_app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth.views import login
 from rome_app import views
