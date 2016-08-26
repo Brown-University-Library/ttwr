82e46110f879000e25e31f76646c5ac7acea50e4
Ben Cail <benjamin_cail@brown.edu>
2016-08-26 11:04:12 -0400

    remove DJANGO_SETTINGS_MODULE from run_integration_tests.py - it'll just use whatever settings module you've configured in your env

--- run_integration_tests.py
+++ run_integration_tests.py
@@ -7,7 +7,6 @@ from django.test.utils import get_runner
 
 
 if __name__ == '__main__':
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
