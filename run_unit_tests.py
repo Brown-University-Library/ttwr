3dad923def8ba0d214ebaf37de45d5432a96d3c9
Ben Cail <benjamin_cail@brown.edu>
2017-02-15 12:19:50 -0500

    use localhost for unit tests

--- run_unit_tests.py
+++ run_unit_tests.py
@@ -8,7 +8,7 @@ from django.test.utils import get_runner
 
 if __name__ == '__main__':
     os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings.unit_tests'
-    os.environ['ROME_BDR_SERVER'] ='ttwr_rome_example.org'
+    os.environ['ROME_BDR_SERVER'] ='localhost'
     os.environ['ROME_PID_PREFIX'] ='testsuite'
     os.environ['ROME_BDR_IDENTITY'] ='ID1'
     os.environ['ROME_BDR_AUTH_CODE'] = '12345'
