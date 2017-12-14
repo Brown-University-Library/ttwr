fba17f7bd5500e7bbcf78df45cd16a62128e262f
Ben Cail <benjamin_cail@brown.edu>
2017-12-14 16:40:16 -0500

    fix error in passenger_wsgi.py

--- config/passenger_wsgi.py
+++ config/passenger_wsgi.py
@@ -5,7 +5,7 @@ import dotenv
 SITE_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 path.append(SITE_ROOT)
 
-dotenv.read_dotenv(join(SITE_ROOT, '.env'))
+dotenv.read_dotenv(os.path.join(SITE_ROOT, '.env'))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
