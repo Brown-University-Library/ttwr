c91eee7792e4c5740e342d2bbb0f98c66a83b04b
Ben Cail <benjamin_cail@brown.edu>
2021-03-05 10:26:18 -0500

    move .env up to project root

--- config/passenger_wsgi.py
+++ config/passenger_wsgi.py
@@ -3,9 +3,11 @@ from sys import path
 import dotenv
 
 SITE_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-path.append(SITE_ROOT)
+if SITE_ROOT not in path:
+    path.append(SITE_ROOT)
 
-dotenv.read_dotenv(os.path.join(SITE_ROOT, '.env'))
+PROJECT_ROOT = dirname(SITE_ROOT)
+dotenv.read_dotenv(os.path.join(PROJECT_ROOT, '.env'))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
