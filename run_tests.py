e3deec5d496d297d114442edadd4ff575310ed74
Ben Cail <benjamin_cail@brown.edu>
2021-03-05 10:18:32 -0500

    update logging

--- run_tests.py
+++ run_tests.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys 
-#import warnings
+import tempfile
 
 
 if __name__ == '__main__':
-    #warnings.filterwarnings('error')
     import django
     from django.conf import settings
     from django.test.utils import get_runner
@@ -14,8 +13,11 @@ if __name__ == '__main__':
     os.environ['ROME_PID_PREFIX'] ='testsuite'
     os.environ['ROME_BDR_IDENTITY'] ='ID1'
     os.environ['ROME_BDR_AUTH_CODE'] = '12345'
-    django.setup()
-    TestRunner = get_runner(settings)
-    test_runner = TestRunner()
-    failures = test_runner.run_tests(['unit_tests'])
+    os.environ['ROME_BDR_AUTH_CODE'] = '12345'
+    with tempfile.TemporaryDirectory() as tmp:
+        os.environ['LOG_DIR'] = tmp
+        django.setup()
+        TestRunner = get_runner(settings)
+        test_runner = TestRunner()
+        failures = test_runner.run_tests(['unit_tests'])
     sys.exit(bool(failures))
