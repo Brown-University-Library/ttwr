d1d3f155f5c0ea9d47f903bbb895c997adacc143
Ben Cail <benjamin_cail@brown.edu>
2021-03-05 11:05:44 -0500

    fix dirname call

--- config/passenger_wsgi.py
+++ config/passenger_wsgi.py
@@ -6,7 +6,7 @@ SITE_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if SITE_ROOT not in path:
     path.append(SITE_ROOT)
 
-PROJECT_ROOT = dirname(SITE_ROOT)
+PROJECT_ROOT = os.path.dirname(SITE_ROOT)
 dotenv.read_dotenv(os.path.join(PROJECT_ROOT, '.env'))
 
 from django.core.wsgi import get_wsgi_application
