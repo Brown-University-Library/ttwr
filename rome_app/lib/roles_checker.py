b371ff5c588a9c9bae10a146f79b78221228ce1d
Birkin James Diana <birkin.diana@gmail.com>
2023-04-27 00:18:17 -0400

    creates problem output.

--- rome_app/lib/roles_checker.py
+++ rome_app/lib/roles_checker.py
@@ -5,18 +5,30 @@ Usage:
 - cd ./ttwr
 - export ROME__PROJECT_DIR_ROOT_PATH="/full/path/to/stuff/github_dir/"
 - export ROME__DOTENV_PATH="/full/path/to/stuff/.env"
+- export ROME__LOG_LEVEL="DEBUG"
 - source ../env/bin/activate
 - python ./lib/roles_checker.py
 
 Sample output:
 Roles in Biography/Roles that are not in the Roles table:
-{ 'name': 'Person_A', 'id': 1, 'invalid_roles: ['bad_role_A', 'bad_role_B'] }
-{ 'name': 'Person_B', 'id': 2, 'invalid_roles: ['bad_role_C'] }
+{ 'name': 'Person_A', 'id': 1, 'invalid_roles': ['bad_role_A', 'bad_role_B'] }
+{ 'name': 'Person_B', 'id': 2, 'invalid_roles': ['bad_role_C'] }
 etc...
 """
 
 import logging, os, pathlib, pprint, sys
 import django, dotenv
+
+level_dict = { 'DEBUG': logging.DEBUG, 'INFO': logging.INFO }
+ENVAR_LOG_LEVEL = os.environ['ROME__LOG_LEVEL']
+print( f'ENVAR_LOG_LEVEL, ``{ENVAR_LOG_LEVEL}``' )
+LEVEL_OBJECT = level_dict[ ENVAR_LOG_LEVEL ]
+logging.basicConfig(
+    level=LEVEL_OBJECT,
+    format='[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s', 
+    datefmt='%d/%b/%Y %H:%M:%S' )
+log = logging.getLogger( 'example_script' )
+log.debug( 'starting log' )
 
 
 ## run code ---------------------------------------------------------
@@ -24,24 +36,29 @@ def run_code():
     """ Runs code.
         Called by `__main__`. """
     from rome_app.models import Biography, Role
-    log.info( 'starting run_code()' )
+    log.debug( 'starting run_code()' )
     bios = Biography.objects.all()
     problems = []
     for (i, bio) in enumerate( bios ):
-        log.info( f'checking bio.name, ``{bio.name}``' )
+        error_entry = { 'name': bio.name, 'id': bio.id, 'invalid_roles': [] }
+        log.debug( f'checking bio.name, ``{bio.name}``' )
         roles = bio.roles
-        log.info( f'roles, ``{roles}``' )
+        log.debug( f'roles, ``{roles}``' )
         split_roles = []
         if roles:
             split_roles = roles.split( ';' )
-        log.info( f'split_roles, ``{split_roles}``' )
+        log.debug( f'split_roles, ``{split_roles}``' )
         validity = 'init'
         for role in split_roles:
             validity = check_role( role )
-            # log.debug( f'validity, ``{validity}``' )
+            if validity == 'invalid':
+                error_entry['invalid_roles'].append( role )
+        if error_entry['invalid_roles']:
+            problems.append( error_entry )
         if i > 10:
             break
-    log.info( 'end of run_code()' )
+    log.info( f'problems, ``{pprint.pformat(problems, sort_dicts=False)}``' )
+    log.debug( 'end of run_code()' )
     return
 
 
@@ -49,47 +66,39 @@ def check_role( role_to_check: str ):
     """ Checks biography-role against Roles table.
         Called by `run_code()`. """
     from rome_app.models import Role
-    log.info( f'role_to_check initially, ``{role_to_check}``' )
+    log.debug( f'role_to_check initially, ``{role_to_check}``' )
     role_to_check = role_to_check.strip()
-    log.info( f'role_to_check stripped, ``{role_to_check}``' )
+    log.debug( f'role_to_check stripped, ``{role_to_check}``' )
     try:
         role_lookup = Role.objects.get( text=role_to_check )
-        log.info( f'type(role_lookup), ``{type(role_lookup)}``' )
+        log.debug( f'type(role_lookup), ``{type(role_lookup)}``' )
         validity_check = 'valid'
     except Exception as e:
-        log.info( f'exception, ``{e}``')
-        log.info( f'role, ``{role_to_check}`` not found' )
+        log.debug( f'exception, ``{e}``')
+        log.debug( f'role, ``{role_to_check}`` not found' )
         validity_check = 'invalid'
-    log.info( f'validity_check, ``{validity_check}``' )
+    log.debug( f'validity_check, ``{validity_check}``' )
     return validity_check
 
 
-## helper -- setup logging ------------------------------------------
-""" Called by `__main__`. """
-logging.basicConfig(
-    level=logging.INFO,
-    format='[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s', 
-    datefmt='%d/%b/%Y %H:%M:%S' )
-log = logging.getLogger( 'example_script' )
-log.info( 'starting log' )
 
 
 ## helper -- setup environment --------------------------------------
 def setup_environment():
     """ Updates sys.path and reads the .env settings.
         Called by `__main__`. """
-    log.info( 'setting up environment' )
+    log.debug( 'setting up environment' )
     ## allows bdr_tools to be imported ------------------------------
     PROJECT_ROOT = os.environ['ROME__PROJECT_DIR_ROOT_PATH']
-    log.info( f'PROJECT_ROOT, ``{PROJECT_ROOT}``' )
+    log.debug( f'PROJECT_ROOT, ``{PROJECT_ROOT}``' )
     if PROJECT_ROOT not in sys.path:
         sys.path.append( PROJECT_ROOT )
     ## loads .env settings ------------------------------------------
     DOTENV_PATH = os.environ['ROME__DOTENV_PATH']
-    log.info( f'DOTENV_PATH, ``{DOTENV_PATH}``' )
+    log.debug( f'DOTENV_PATH, ``{DOTENV_PATH}``' )
     try:
         dotenv.read_dotenv( DOTENV_PATH )
-        log.info( 'dotenv successfully read' ) 
+        log.debug( 'dotenv successfully read' ) 
     except Exception as e:
         log.exception( 'problem reading dotenv' )
     ## loads django --------------------------------------------------
@@ -98,16 +107,16 @@ def setup_environment():
     if cwd not in sys.path:
         sys.path.append( cwd )
     django.setup()      # ok, now django-related imports will work
-    log.info( 'django.setup() complete' )
+    log.debug( 'django.setup() complete' )
     return
 
 
 ## caller -----------------------------------------------------------
 if __name__ == "__main__":
-    log.info( 'starting if __name__ == "__main__"')
+    log.debug( 'starting if __name__ == "__main__"')
     setup_environment()     # loads .env settings and envars
     run_code()              # THIS IS WHERE WORK IS DONE   
-    log.info( 'eof' )
+    log.debug( 'eof' )
 
 
 ## eof
