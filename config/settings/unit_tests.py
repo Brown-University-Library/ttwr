edb469dad35283a86fe702c8e63e62aaff0aa924
Birkin James Diana <birkin.diana@gmail.com>
2025-05-19 15:55:59 -0400

    reverts experimental change.

--- config/settings/unit_tests.py
+++ config/settings/unit_tests.py
@@ -17,26 +17,6 @@ DEBUG = True
 
 ADMINS = (('random', 'random@example.com'),)
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
-#         'NAME': os.getenv('DB_NAME', 'unit_tests.sqlite3'),
-#         'USER': os.getenv('DB_USER', ''),
-#         'PASSWORD': os.getenv('DB_PASSWORD', ''),
-#         'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-#         'PORT': os.getenv('DB_PORT', ''),
-#         'TEST': {
-#             'ENGINE': os.getenv('TEST_DB_ENGINE', 'django.db.backends.sqlite3'),
-#             'NAME': os.getenv('TEST_DB_NAME', 'unit_tests.sqlite3'),
-#             'USER': os.getenv('TEST_DB_USER'),
-#             'PASSWORD': os.getenv('TEST_DB_PASSWORD'),
-#             'HOST': os.getenv('TEST_DB_HOST'),
-#             'PORT': int(os.getenv('TEST_DB_PORT', 3306)),
-#         },
-#     }
-# }
-# pprint.pprint(DATABASES)
-
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
@@ -45,9 +25,18 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', '127.0.0.1'),
         'PORT': os.getenv('DB_PORT', ''),
+        'TEST': {
+            'ENGINE': os.getenv('TEST_DB_ENGINE', 'django.db.backends.sqlite3'),
+            'NAME': os.getenv('TEST_DB_NAME', 'unit_tests.sqlite3'),
+            'USER': os.getenv('TEST_DB_USER'),
+            'PASSWORD': os.getenv('TEST_DB_PASSWORD'),
+            'HOST': os.getenv('TEST_DB_HOST'),
+            'PORT': int(os.getenv('TEST_DB_PORT', 3306)),
+        },
     }
 }
 pprint.pprint(DATABASES)
+
 
 SECRET_KEY = '1234567890'
 STATIC_URL = '/static/'
