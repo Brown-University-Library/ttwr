5884e54e1cb49cba53c428a3ec209d7166cd4530
Birkin James Diana <birkin.diana@gmail.com>
2025-05-19 18:50:48 -0400

    prints db-engine info.

--- run_tests.py
+++ run_tests.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python
 import os
-import sys 
+import sys
 import tempfile
 
+from django.db import connection
 
 if __name__ == '__main__':
     import django
     from django.conf import settings
     from django.test.utils import get_runner
+
     os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings.unit_tests'
-    os.environ['ROME_BDR_SERVER'] ='localhost'
-    os.environ['ROME_PID_PREFIX'] ='testsuite'
-    os.environ['ROME_BDR_IDENTITY'] ='ID1'
+    os.environ['ROME_BDR_SERVER'] = 'localhost'
+    os.environ['ROME_PID_PREFIX'] = 'testsuite'
+    os.environ['ROME_BDR_IDENTITY'] = 'ID1'
     os.environ['ROME_BDR_AUTH_CODE'] = '12345'
     os.environ['ROME_BDR_AUTH_CODE'] = '12345'
     with tempfile.TemporaryDirectory() as tmp:
         os.environ['LOG_DIR'] = tmp
         django.setup()
+
+        print(
+            f'ACTUAL DATABASE ENGINE IN USE: ``{connection.vendor}``, settings.ENGINE={connection.settings_dict["ENGINE"]}'
+        )
+
         TestRunner = get_runner(settings)
         test_runner = TestRunner()
         failures = test_runner.run_tests(['unit_tests'])
