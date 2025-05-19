b84d62da69e6b49570fe02e448dbf2c8f7305005
Birkin James Diana <birkin.diana@gmail.com>
2025-05-19 15:00:35 -0400

    adds empty keys to DATABASES setting.

--- config/settings/unit_tests.py
+++ config/settings/unit_tests.py
@@ -30,6 +30,11 @@ ADMINS = (('random', 'random@example.com'),)
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.getenv('DB_NAME', 'unit_tests.sqlite3'),
+        'USER': os.getenv('DB_USER', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', ''),
         'TEST': {
             'ENGINE': os.getenv('TEST_DB_ENGINE', 'django.db.backends.sqlite3'),
             'NAME': os.getenv('TEST_DB_NAME', 'unit_tests.sqlite3'),
