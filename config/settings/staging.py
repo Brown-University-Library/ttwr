b8e827b6dcc55e477c48915d240067343701b581
Birkin James Diana <birkin.diana@gmail.com>
2025-05-19 21:14:59 -0400

    adds turnstile settings.

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -1,4 +1,5 @@
-from .base import *
+from .base import *  # noqa: F403
+from .base import get_env_setting
 
 DEBUG = True
 
@@ -22,3 +23,13 @@ EMAIL_HOST = get_env_setting('EMAIL_HOST')
 ALLOWED_HOSTS = [get_env_setting('ALLOWED_HOST')]
 TTWR_COLLECTION_PID = 'test:5m6nkymr'
 SERVER_EMAIL = get_env_setting('SERVER_EMAIL')
+
+## turnstile settings ---------------------------
+TURNSTILE_SITE_KEY = get_env_setting('TURNSTILE_SITE_KEY')
+TURNSTILE_SECRET_KEY = get_env_setting('TURNSTILE_SECRET_KEY')
+TURNSTILE_API_URL = get_env_setting('TURNSTILE_API_URL')
+TURNSTILE_API_TIMEOUT = get_env_setting('TURNSTILE_API_TIMEOUT')
+TURNSTILE_API_MAX_RETRIES = get_env_setting('TURNSTILE_API_MAX_RETRIES')
+TURNSTILE_API_RETRY_DELAY = get_env_setting('TURNSTILE_API_RETRY_DELAY')
+
+
