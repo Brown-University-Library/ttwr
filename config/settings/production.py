31a8bd525cb44bebb4f75a4f438c2091bc427462
Birkin James Diana <birkin.diana@gmail.com>
2025-05-19 23:21:31 -0400

    adds turnstile prod-settings.

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,7 @@
 import json
 
-from .base import *
+from config.settings.base import *  # noqa: F403
+from config.settings.base import MIDDLEWARE, get_env_setting
 
 DEBUG = False
 
@@ -20,8 +21,19 @@ DATABASES = {
     }
 }
 
+## add an entry to MIDDLEWARE
+MIDDLEWARE = MIDDLEWARE + ['config.middleware.turnstile_middleware.TurnstileMiddleware']
+
 SECRET_KEY = get_env_setting('SECRET_KEY')
 STATIC_URL = '/projects/rome/static/'
 ALLOWED_HOSTS = ['library.brown.edu']
 EMAIL_HOST = get_env_setting('EMAIL_HOST')
 SERVER_EMAIL = get_env_setting('SERVER_EMAIL')
+
+## turnstile settings ---------------------------
+TURNSTILE_SITE_KEY = get_env_setting('TURNSTILE_SITE_KEY')
+TURNSTILE_SECRET_KEY = get_env_setting('TURNSTILE_SECRET_KEY')
+TURNSTILE_API_URL = get_env_setting('TURNSTILE_API_URL')
+TURNSTILE_API_TIMEOUT = int(get_env_setting('TURNSTILE_API_TIMEOUT'))
+TURNSTILE_EMAIL = get_env_setting('TURNSTILE_EMAIL')
+TURNSTILE_SESSION_EXPIRY_MINUTES = int(get_env_setting('TURNSTILE_SESSION_EXPIRY_MINUTES'))
