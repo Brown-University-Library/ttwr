2100caccd7a7e8eb1dc7d2d4bbb754ece3f4ad0d
Birkin James Diana <birkin.diana@gmail.com>
2025-05-19 20:42:32 -0400

    adds turnstile to urls.py.

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,12 @@
-from django.urls import include, re_path
+from django.conf.urls import url
 from django.contrib import admin
+from django.urls import include, re_path
+
+from config.middleware.turnstile_view import turnstile_verify
 
 admin.autodiscover()
 
 urlpatterns = [
-  re_path( r'^',  include('rome_app.urls_app') ),
+    url(r'^turnstile-verify/', turnstile_verify, name='turnstile-verify'),  # turnstile verify endpoint
+    re_path(r'^', include('rome_app.urls_app')),
 ]
